19b602e776509f9794c92524e4ca727e991ba391
loonelis <egortopcki@gmail.com>
2024-05-30 12:39:53 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+
+from contextlib import asynccontextmanager
+
+from database import create_tables
+from router import router as tasks_router
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_tables()
+    print('База готова к работе')
+    yield
+    print('Выключение!')
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
+
+
+
